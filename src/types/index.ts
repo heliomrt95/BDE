43b90d83dc584ceb1b6d95a0c3717b533e77d1d4
helio martone <helio.martone@mmibordeaux.com>
2026-03-31 15:39:46 +0200

    refactor: remove lunch-related components and types

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,6 +4,5 @@ export * from './event';
 export * from './portfolio';
 export * from './blog';
 export * from './merch';
-export * from './lunch';
 export * from './program';
 export * from './user';
